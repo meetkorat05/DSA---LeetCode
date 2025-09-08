548040b6bd58c72e7d4773df5df13c171d8de822
Meet Korat <meetkorat2105@gmail.com>
2025-09-08 07:40:00 +0530

    Time: 15 ms (61.95%) | Memory: 12.5 MB (8.20%) - LeetSync

---
+++ 278-first-bad-version/first-bad-version.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def firstBadVersion(self, n):
+        low = 1
+        high = n
+        while low <= high:
+            mid = (low+high)//2
+            if isBadVersion(mid):
+                high = mid - 1
+            else:
+                low = mid + 1
+        return low
+        
