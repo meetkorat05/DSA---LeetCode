2f160a3117deaf32b4751659b3ddc7dfeced3af4
Meet Korat <meetkorat2105@gmail.com>
2025-09-23 18:27:06 +0530

    Time: 11 ms (18.51%) | Memory: 12.4 MB (50.14%) - LeetSync

---
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.py
@@ -0,0 +1,18 @@
+from collections import Counter
+
+class Solution(object):
+    def maxFreqSum(self, s):
+        vowels = set("aeiou")
+        freq = Counter(s)
+
+        max_vowel = 0
+        max_consonant = 0
+
+        for ch, count in freq.items():
+            if ch in vowels:
+                max_vowel = max(max_vowel, count)
+            else:
+                max_consonant = max(max_consonant, count)
+
+        return max_vowel + max_consonant
+        
